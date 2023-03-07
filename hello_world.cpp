e42e72383005703aaebdeda95d67651646578a32
Green-Ace <bezlepkin.s04@gmail.com>
2023-03-07 21:40:14 +0300

    created comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main(){
-	std::string name;
+	std::string name; // создаем переменную
 	std::cout << "Your name:" << std::endl;
-	std::cin >> name;
-	std::cout << "Hello world from " << name;
+	std::cin >> name; // ввод имени в переменную
+	std::cout << "Hello world from " << name; //вывод программы
 	return 0;
 }
